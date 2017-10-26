ed519055784e49c1ed4e7264793866cb73c693cf
Asdqwe258 <31545502+Asdqwe258@users.noreply.github.com>
2017-10-26 13:05:15 -0400

    Update Pokemon.java

--- Pokemon.java
+++ Pokemon.java
@@ -1,10 +1,10 @@
 import java.util.Random;
 public class Pokemon {
 
-	public void atk(Pokemon enemy){
-		= enemy.getHealth() - (11/25)*(Pokemon1.getAtk()/enemy.getDef()) + 2
+	public void atk(Pokemon a, Pokemon b){
+		= b.getHealth() - (11/25)*(a.getAtk()/b.getDef()) + 2
 	}
-	public void spAtk(Pokemon enemy){
+	public void spAtk(Pokemon a,Pokemon b){
 		
 	}
 }
