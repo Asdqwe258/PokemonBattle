aa058287486d591c211d69a89cf997cf88df378e
Asdqwe258 <31545502+Asdqwe258@users.noreply.github.com>
2017-10-26 13:02:14 -0400

    Update WaterType.java

--- WaterType.java
+++ WaterType.java
@@ -3,15 +3,36 @@ public class WaterType extends Pokemon{
 	int atk;
 	int spatk;
 	int def;
-	int spdf;
+	int spdef;
 	int speed;
-	public WaterType(double a, int b, int c, int d, int e, int f){
+	public GrassType(double a, int b, int c, int d, int e, int f){
 		health = a;
 		atk = b;
 		spatk = c;
 		def = d;
-		spdf = e;
-		speed = ;
+		spdef = e;
+		speed = f;
 	}
-}
+	public double getHealth(){
+		return health;
+	}
+	public int getAtk(){
+		return atk;
+	}
+	public double getspAtk(){
+		return spatk;
+	}
+	public double getDef(){
+		return def;
+	}
+	public double getspDef(){
+		return spdef;
+	}
+	public double getSpeed(){
+		return speed;
+	}
+	public String toString(){
+		return ("Health: " + health + "\nAttack: " + atk + "\nSpecial Attack: " + "\nDefense: " + def + "\nSpecial Defense: " + spdef + "\nSpeed: " + speed);
+	}
 	
+}
