e0d5d10db7f963770eac180ecdc8139109b460e4
Asdqwe258 <31545502+Asdqwe258@users.noreply.github.com>
2017-10-26 13:19:19 -0400

    moved methods to pokemon

--- Pokemon.java
+++ Pokemon.java
@@ -1,11 +1,45 @@
 import java.util.Random;
-public class Pokemon {
-
+public class Pokemon{
+	double health;
+	int atk;
+	int spatk;
+	int def;
+	int spdef;
+	int speed;
+	public Pokemon(double a, int b, int c, int d, int e, int f) {
+		health = a;
+		atk = b;
+		spatk = c;
+		def = d;
+		spdef = e;
+		speed = f;
+	}
 	public void atk(Pokemon a, Pokemon b){
-		health = b.getHealth() - (11/25)*(a.getAtk()/b.getDef()) + 2
+		health = b.getHealth() - (11/25)*(a.getAtk()/b.getDef()) + 2;
 	}
 	public void spAtk(Pokemon a,Pokemon b){
-		health = b.getHealth() - (11/25)*(a.getSpAtk()/b.getSpDef()) + 2
-
+		health = b.getHealth() - (11/25)*(a.getSpAtk()/b.getSpDef()) + 2;
 	}
+	public double getHealth(){
+		return health;
+	}
+	public int getAtk(){
+		return atk;
+	}
+	public double getSpAtk(){
+		return spatk;
+	}
+	public double getDef(){
+		return def;
+	}
+	public double getSpDef(){
+		return spdef;
+	}
+	public double getSpeed(){
+		return speed;
+	}
+	public String toString(){
+		return ("Health: " + health + "\nAttack: " + atk + "\nSpecial Attack: " + "\nDefense: " + def + "\nSpecial Defense: " + spdef + "\nSpeed: " + speed);
+	}
+	
 }
