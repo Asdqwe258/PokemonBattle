d6942c998aa942ba3de923b1d833553d79655159
AnjaliSardana <33128801+AnjaliSardana@users.noreply.github.com>
2017-10-26 13:08:46 -0400

    Update Pokemon.java

--- Pokemon.java
+++ Pokemon.java
@@ -5,6 +5,7 @@ public class Pokemon {
 		= b.getHealth() - (11/25)*(a.getAtk()/b.getDef()) + 2
 	}
 	public void spAtk(Pokemon a,Pokemon b){
-		
+		= b.getHealth() - (11/25)*(a.getSpAtk()/b.getSpDef()) + 2
+
 	}
 }
