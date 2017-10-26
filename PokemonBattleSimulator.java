43ee0307c696cc2af6c5c6afb85385c7e2389596
AnjaliSardana <33128801+AnjaliSardana@users.noreply.github.com>
2017-10-26 13:20:43 -0400

    Update PokemonBattleSimulator.java

--- PokemonBattleSimulator.java
+++ PokemonBattleSimulator.java
@@ -1,1 +1,10 @@
-public class
+import java.util.Scanner;
+public class PokemonBattleSimulator{
+        public static void main(String args[]) {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Please choose the first Pokemon type: If you want to choose a Grass type press 1, if you want to choose a Fire type press 2, if you want to choose a Water type press 3");
+        FirstPokemonType = keyboard.nextInt();
+        System.out.print("Please choose the second Pokemon type: If you want to choose a Grass type press 1, if you want to choose a Fire type press 2, if you want to choose a Water type press 3");
+        SecondPokemonType = keyboard.nextInt();
+        if(
+
