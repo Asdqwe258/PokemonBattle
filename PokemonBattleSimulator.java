7fb71ae5875c17f47d7dd832edcdda335ef28035
AnjaliSardana <33128801+AnjaliSardana@users.noreply.github.com>
2017-10-29 13:14:29 -0400

    Update PokemonBattleSimulator.java

--- PokemonBattleSimulator.java
+++ PokemonBattleSimulator.java
@@ -6,5 +6,14 @@ public class PokemonBattleSimulator{
         FirstPokemonType = keyboard.nextInt();
         System.out.print("Please choose the second Pokemon type: If you want to choose a Grass type press 1, if you want to choose a Fire type press 2, if you want to choose a Water type press 3");
         SecondPokemonType = keyboard.nextInt();
-        if(
+        int advantage = 0;
+        if(FirstPokemonType == SecondPokemonType){
+                advantage = 1;
+        }
+        if((FirstPokemonType == 1 && SecondPokemonType == 3)||(FirstPokemonType == 2 && SecondPokemonType == 1) || (FirstPokemonType == 3 && SecondPokemonType == 2)){
+                advantage = 2;
+        }
+        if((FirstPokemonType == 3 && SecondPokemonType == 1)||(FirstPokemonType == 1 && SecondPokemonType == 2) || (FirstPokemonType == 2 && SecondPokemonType == 3))
+                advantage = .5;
+        }
 
