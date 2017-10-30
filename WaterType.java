31aaf6eb29fca2954e835beb8de1e295e8aafde1
AnjaliSardana <33128801+AnjaliSardana@users.noreply.github.com>
2017-10-30 13:10:42 -0400

    Update WaterType.java

--- WaterType.java
+++ WaterType.java
@@ -1,6 +1,6 @@
 public class WaterType extends Pokemon{
 
-	public GrassType(double a, int b, int c, int d, int e, int f){
+	public WaterType(double a, int b, int c, int d, int e, int f){
 		super(a, b, c, d, e, f);
 		type = 3;
 	}
