34abc7a1790827cc420a0656e6b08786808d6a13
AnjaliSardana <33128801+AnjaliSardana@users.noreply.github.com>
2017-10-30 13:18:47 -0400

    Update PokemonBattleSimulator.java

--- PokemonBattleSimulator.java
+++ PokemonBattleSimulator.java
@@ -20,5 +20,5 @@ public class PokemonBattleSimulator{
         Pokemon ababa = new Pokemon(keyboard.nextDouble(), keyboard.nextInt(),keyboard.nextInt(),keyboard.nextInt(),keyboard.nextInt(),keyboard.nextInt());
         System.out.println("Please enter the stats of your second pokemon: ");
         Pokemon cdcdc = new Pokemon(keyboard.nextDouble(), keyboard.nextInt(),keyboard.nextInt(),keyboard.nextInt(),keyboard.nextInt(),keyboard.nextInt());
-
+        System.out.println("Choose your next action. Press 1 to attack or 2 to special attack:");
 
