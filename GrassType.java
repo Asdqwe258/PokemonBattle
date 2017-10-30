f9c32af9ca90285a7a789656da72fc615e5a3fc5
Asdqwe258 <31545502+Asdqwe258@users.noreply.github.com>
2017-10-30 12:56:34 -0400

    Big change, moved methods

--- GrassType.java
+++ GrassType.java
@@ -1,38 +1,7 @@
 public class GrassType extends Pokemon{
-	double health;
-	int atk;
-	int spatk;
-	int def;
-	int spdef;
-	int speed;
+
 	public GrassType(double a, int b, int c, int d, int e, int f){
-		health = a;
-		atk = b;
-		spatk = c;
-		def = d;
-		spdef = e;
-		speed = f;
+		super(a, b, c, d, e, f);
+		type = 1;
 	}
-	public double getHealth(){
-		return health;
-	}
-	public int getAtk(){
-		return atk;
-	}
-	public double getSpAtk(){
-		return spatk;
-	}
-	public double getDef(){
-		return def;
-	}
-	public double getSpDef(){
-		return spdef;
-	}
-	public double getSpeed(){
-		return speed;
-	}
-	public String toString(){
-		return ("Health: " + health + "\nAttack: " + atk + "\nSpecial Attack: " + "\nDefense: " + def + "\nSpecial Defense: " + spdef + "\nSpeed: " + speed);
-	}
-	
 }
