927cdc50e3e3fc299e9b5954967e73d61509abee
AnjaliSardana <33128801+AnjaliSardana@users.noreply.github.com>
2017-10-30 13:10:21 -0400

    Update FireType.java

--- FireType.java
+++ FireType.java
@@ -1,6 +1,6 @@
 public class FireType extends Pokemon{
 
-	public GrassType(double a, int b, int c, int d, int e, int f){
+	public FireType(double a, int b, int c, int d, int e, int f){
 		super(a, b, c, d, e, f);
 		type = 2;
 	}
