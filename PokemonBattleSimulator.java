378f0c7575b25184f45447a1262b6491f209eeba
Asdqwe258 <31545502+Asdqwe258@users.noreply.github.com>
2017-11-01 12:49:54 -0400

    Update PokemonBattleSimulator.java

--- PokemonBattleSimulator.java
+++ PokemonBattleSimulator.java
@@ -4,19 +4,19 @@ public class PokemonBattleSimulator{
         public static void main(String args[]) {
         Random random = new Random();
         Scanner keyboard = new Scanner(System.in);
-        System.out.print("Please choose the first Pokemon type: If you want to choose a Grass type press 1, if you want to choose a Fire type press 2, if you want to choose a Water type press 3");
-        FirstPokemonType = keyboard.nextInt();
-        System.out.print("Please choose the second Pokemon type: If you want to choose a Grass type press 1, if you want to choose a Fire type press 2, if you want to choose a Water type press 3");
-        SecondPokemonType = keyboard.nextInt();
+        System.out.print("Please choose the first Pokemon type: \nIf you want to choose a Grass type press 1 \nif you want to choose a Fire type press 2 \nif you want to choose a Water type press 3");
+        int FirstPokemonType = keyboard.nextInt();
+        System.out.print("Please choose the second Pokemon type: If you want to choose a Grass type press 1 \nif you want to choose a Fire type press 2 \nif you want to choose a Water type press 3");
+        int SecondPokemonType = keyboard.nextInt();
         double advantage = 0;
         if(FirstPokemonType == SecondPokemonType){
-                advantage = 1.0;
+               advantage = 1.0;
         }
         if((FirstPokemonType == 1 && SecondPokemonType == 3)||(FirstPokemonType == 2 && SecondPokemonType == 1) || (FirstPokemonType == 3 && SecondPokemonType == 2)){
-                advantage = 2.0;
+               advantage = 2.0;
         }
-        if((FirstPokemonType == 3 && SecondPokemonType == 1)||(FirstPokemonType == 1 && SecondPokemonType == 2) || (FirstPokemonType == 2 && SecondPokemonType == 3))
-                advantage = .5;
+        if((FirstPokemonType == 3 && SecondPokemonType == 1)||(FirstPokemonType == 1 && SecondPokemonType == 2) || (FirstPokemonType == 2 && SecondPokemonType == 3)){
+               advantage = .5;
         }
         System.out.println("Please enter the stats of your first pokemon: ");
         Pokemon ababa = new Pokemon(keyboard.nextDouble(), keyboard.nextInt(),keyboard.nextInt(),keyboard.nextInt(),keyboard.nextInt(),keyboard.nextInt(),FirstPokemonType);
@@ -24,33 +24,40 @@ public class PokemonBattleSimulator{
         Pokemon cdcdc = new Pokemon(keyboard.nextDouble(), keyboard.nextInt(),keyboard.nextInt(),keyboard.nextInt(),keyboard.nextInt(),keyboard.nextInt(),SecondPokemonType);
               
         while(ababa.getHealth() > 0 && cdcdc.getHealth() > 0){
-                vary = random.nextDouble();
+                double vary = random.nextDouble()*.15+.85;
                 System.out.println("Choose your next action for Pokemon 1. Press 1 to attack or 2 to special attack:");
-                nextAction1 = keyboard.nextInt();
-                System.out.println("Choose your next action for Pokemon 1. Press 1 to attack or 2 to special attack:");
-                nextAction2 = keyboard.nextInt();
+                int nextAction1 = keyboard.nextInt();
+                System.out.println("Choose your next action for Pokemon 2. Press 1 to attack or 2 to special attack:");
+                int nextAction2 = keyboard.nextInt();
                 if(ababa.getSpeed() > cdcdc.getSpeed()){
+                	System.out.println("Pokemon 1 Attacks!");
                         ababa.atk(cdcdc,nextAction1,advantage,vary);
+                        System.out.println("Pokemon 2's health is now: " + cdcdc.getHealth());
                         if(cdcdc.getHealth() < 0){
                              break;
                         }
-                        cdcdc.atk(ababa,nextAction2,1/advantage,vary);                                                               
+                        System.out.println("Pokemon 2 Attacks!");
+                        cdcdc.atk(ababa,nextAction2,1/advantage,vary);     
+                        System.out.println("Pokemon 1's health is now: " + ababa.getHealth());
                 }
                 else if(ababa.getSpeed() < cdcdc.getSpeed()){
+                	System.out.println("Pokemon 2 Attacks!");
                         cdcdc.atk(ababa,nextAction2,1/advantage,vary);
+                        System.out.println("Pokemon 1's health is now: " + ababa.getHealth());
                         if(ababa.getHealth() < 0){
                                 break;
                         }
+                        System.out.println("Pokemon 1 Attacks!");
                         ababa.atk(cdcdc,nextAction1,advantage,vary);
+                        System.out.println("Pokemon 2's health is now: " + cdcdc.getHealth());
                 }
         }
-        if(ababa.getHealth < 0){
+        if(ababa.getHealth() < 0){
                 System.out.println("Pokemon 1 loses!");
         }
         else{
                 System.out.println("Pokemon 2 loses!");
         }
-                     
-                
-               
+}
+}
 
