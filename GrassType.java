aaa2776b7308686c56927c57cf004be3b5cfa860
AnjaliSardana <33128801+AnjaliSardana@users.noreply.github.com>
2017-11-01 13:03:48 -0400

    Update GrassType.java

--- GrassType.java
+++ GrassType.java
@@ -1,6 +1,9 @@
+// Andrew Lee and Anjali Sardana
+// 11/1/17
+//Period 5
 public class GrassType extends Pokemon{
 
-	public GrassType(double a, int b, int c, int d, int e, int f){
+	public GrassType(double a, int b, int c, int d, int e, int f){ // literally does nothing
 		super(a, b, c, d, e, f, 1);
 	}
 }
