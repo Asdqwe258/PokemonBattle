25ac3fb6a173ab6380a1aa2d05e6433b60640d04
AnjaliSardana <33128801+AnjaliSardana@users.noreply.github.com>
2017-11-01 13:03:23 -0400

    Update WaterType.java

--- WaterType.java
+++ WaterType.java
@@ -1,6 +1,9 @@
+// Andrew Lee and Anjali Sardana
+// 11/1/17
+//Period 5
 public class WaterType extends Pokemon{
 
-	public WaterType(double a, int b, int c, int d, int e, int f){
+	public WaterType(double a, int b, int c, int d, int e, int f){ // literally does nothing
 		super(a, b, c, d, e, f, 3);
 	}
 }
